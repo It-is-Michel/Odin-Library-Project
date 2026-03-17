cea7cc8640f9f1ff9e1f842de65b8eb703463c26
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-17 15:04:29 -0300

    chore(chore_RefactorObjectsToClasses): Replace obj with classes

--- script.js
+++ script.js
@@ -1,117 +1,124 @@
-function Book(title, author, summary, pagesRead, totalPages, coverImageFile) {
+class Book {
+
+  constructor(title, author, summary, pagesRead, totalPages, coverImageFile) {
+    this.title = title;
+    this.author = author;
+    this.summary = summary;
+    this.pagesRead = pagesRead;
+    this.totalPages = totalPages;
+    this.coverImageFile = coverImageFile;
+
+    this.#bookElement.classList.add("book");
+
+    this.#titleWrapper.classList.add("book__title-wrapper");
+    this.#bookElement.appendChild(this.#titleWrapper);
+
+    this.#titleElement.classList.add("book__title");
+    this.#titleWrapper.appendChild(this.#titleElement);
+    this.#setTitle(title);
+
+    this.#authorElement.classList.add("book__author");
+    this.#titleWrapper.appendChild(this.#authorElement);
+    this.#setAuthor(author);
+
+    this.#summaryElement.classList.add("book__summary");
+    this.#bookElement.appendChild(this.#summaryElement);
+    this.#setSummary(summary);
+
+    this.#pagesElement.classList.add("book__pages");
+    this.#bookElement.appendChild(this.#pagesElement);
+
+    this.#setPages(totalPages, pagesRead);
+
+    this.#coverElement.classList.add("book__cover");
+    this.#bookElement.appendChild(this.#coverElement);
+    this.#setCoverImg(coverImageFile);
+  };
+
+  #bookElement = document.createElement("article");
   
-  this.title = title,
-  this.author = author,
-  this.summary = summary,
-  this.pagesRead = pagesRead,
-  this.totalPages = totalPages,
-  this.coverImageFile = coverImageFile,
+  get bookElement() {return this.#bookElement};
 
-  this.bookElement = document.createElement("article");
-  this.bookElement.classList.add("book");
+  #titleWrapper = document.createElement("div");
+  #titleElement = document.createElement("h1");
+  #authorElement = document.createElement("cite");
+  #summaryElement = document.createElement("p");
+  #pagesElement = document.createElement("span");
+  #coverElement = document.createElement("div");
 
-  this.titleWrapper = document.createElement("div");
-  this.titleWrapper.classList.add("book__title-wrapper");
-  this.bookElement.appendChild(this.titleWrapper);
-  
-  this.titleElement = document.createElement("h1");
-  this.titleElement.classList.add("book__title");
-  this.titleWrapper.appendChild(this.titleElement);
-  this.setTitle(title);
+  #coverFileReader = new FileReader();
 
-  this.authorElement = document.createElement("cite");
-  this.authorElement.classList.add("book__author");
-  this.titleWrapper.appendChild(this.authorElement);
-  this.setAuthor(author);
+  #setTitle(title) {
+    if (typeof title != "string" || title == "") title = "none";
 
-  this.summaryElement = document.createElement("p");
-  this.summaryElement.classList.add("book__summary");
-  this.bookElement.appendChild(this.summaryElement);
-  this.setSummary(summary);
+    this.#titleElement.textContent = title;
+  };
 
-  this.pagesElement = document.createElement("span");
-  this.pagesElement.classList.add("book__pages");
-  this.bookElement.appendChild(this.pagesElement);
+  #setAuthor(author) {
+    if (typeof author != "string" || author == "") author = "none";
 
-  this.setPages(totalPages, pagesRead);
+    this.#authorElement.textContent = `by ${author}`;
+  };
 
-  this.coverElement = document.createElement("div");
-  this.coverElement.classList.add("book__cover");
-  this.bookElement.appendChild(this.coverElement);
-  this.setCoverImg(coverImageFile);
-}
+  #setSummary(summary) {
+    if (typeof summary != "string") summary = "...";
 
-Book.prototype.setTitle = function(title) {
-  if (typeof title != "string" || title == "") title = "none";
+    this.#summaryElement.textContent = summary;
+  };
 
-  this.titleElement.textContent = title;
-}
+  #setPages(totalPages, readPages) {
+    const totalPagesIsNumber = typeof totalPages == "number";
+    const totalPagesIsPositive = totalPages > 0;
+    totalPages = totalPagesIsNumber && totalPagesIsPositive ? Math.trunc(totalPages) : NaN;
 
-Book.prototype.setAuthor = function(author) {
-  if (typeof author != "string" || author == "") author = "none";
+    const readPagesIsNumber = (typeof readPages == "number");
+    const readPagesIsNotNegative = readPages >= 0;
+    readPages = readPagesIsNumber && readPagesIsNotNegative ? Math.trunc(readPages) : NaN;
+    
+    const pagesTextContent = `${readPages} / ${totalPages} pages`;
+    
+    this.#pagesElement.textContent = pagesTextContent;
 
-  this.authorElement.textContent = `by ${author}`;
-}
+    const readStatus = (Number.isNaN(totalPages) || Number.isNaN(readPages) || readPages > totalPages) ? "Error"
+    : readPages == totalPages ? "Read"
+    : readPages == 0 ? "Not read"
+    : "Reading";
 
-Book.prototype.setSummary = function(summary) {
-  if (typeof summary != "string") summary = "...";
+    const readStatusElement = document.createElement("span");
+    this.#pagesElement.prepend(readStatusElement);
+    readStatusElement.textContent = readStatus;
+    switch (readStatus) {
+      case "Read":
+        readStatusElement.classList.add("book__read-status-read");
+        break;
+      case "Reading":
+        readStatusElement.classList.add("book__read-status-reading");
+        break;
+      case "Not read":
+        readStatusElement.classList.add("book__read-status-not-read");
+        break;
+      case "Error":
+        readStatusElement.classList.add("book__read-status-error");
+        break;
+      default:
+        console.error(`There was a problem when adding a css class to readStatusElement; readStatus value: ${readStatus}; readStatusElement value: ${readStatusElement}`);
+    };
+  };
 
-  this.summaryElement.textContent = summary;
-}
+  #setCoverImg(coverImageFile) {
+    const coverImageFileIsFile = coverImageFile instanceof File;
+    const coverImageFileHasValidFormat = coverImageFileIsFile ? coverImageFile.type.startsWith("image/") : null;
+    if (!coverImageFileIsFile || !coverImageFileHasValidFormat) {
+      this.#coverElement.textContent = "No image";
+      return;
+    }
 
-Book.prototype.setPages = function(totalPages, readPages) {
-  const totalPagesIsNumber = typeof totalPages == "number";
-  const totalPagesIsPositive = totalPages > 0;
-  totalPages = totalPagesIsNumber && totalPagesIsPositive ? Math.trunc(totalPages) : NaN;
-
-  const readPagesIsNumber = (typeof readPages == "number");
-  const readPagesIsNotNegative = readPages >= 0;
-  readPages = readPagesIsNumber && readPagesIsNotNegative ? Math.trunc(readPages) : NaN;
-  
-  const pagesTextContent = `${readPages} / ${totalPages} pages`;
-  
-  this.pagesElement.textContent = pagesTextContent;
-
-  const readStatus = (Number.isNaN(totalPages) || Number.isNaN(readPages) || readPages > totalPages) ? "Error"
-  : readPages == totalPages ? "Read"
-  : readPages == 0 ? "Not read"
-  : "Reading";
-
-  const readStatusElement = document.createElement("span");
-  this.pagesElement.prepend(readStatusElement);
-  readStatusElement.textContent = readStatus;
-  switch (readStatus) {
-    case "Read":
-      readStatusElement.classList.add("book__read-status-read");
-      break;
-    case "Reading":
-      readStatusElement.classList.add("book__read-status-reading");
-      break;
-    case "Not read":
-      readStatusElement.classList.add("book__read-status-not-read");
-      break;
-    case "Error":
-      readStatusElement.classList.add("book__read-status-error");
-      break;
-    default:
-      console.error(`There was a problem when adding a css class to readStatusElement; readStatus value: ${readStatus}; readStatusElement value: ${readStatusElement}`);
+    this.#coverFileReader.onload = (e) => {
+      const coverImageUrl = e.target.result;
+      this.#coverElement.style.backgroundImage = `url("${coverImageUrl}")`;
+    }
+    this.#coverFileReader.readAsDataURL(coverImageFile);
   }
-}
-
-Book.prototype.coverFileReader = new FileReader();
-Book.prototype.setCoverImg = function(coverImageFile) {
-  const coverImageFileIsFile = coverImageFile instanceof File;
-  const coverImageFileHasValidFormat = coverImageFileIsFile ? coverImageFile.type.startsWith("image/") : null;
-  if (!coverImageFileIsFile || !coverImageFileHasValidFormat) {
-    this.coverElement.textContent = "No image";
-    return;
-  }
-
-  this.coverFileReader.onload = (e) => {
-    const coverImageUrl = e.target.result;
-    this.coverElement.style.backgroundImage = `url("${coverImageUrl}")`;
-  }
-  this.coverFileReader.readAsDataURL(coverImageFile);
 }
 
 const LibraryController = {
@@ -360,7 +367,7 @@ const LibraryController = {
       const newBook = this._generateBook(newBookData);
       this._LibraryStorage.addBook(newBook);
     } catch (err) {
-      console.log("User canceled add book");
+      console.log(err);
     }
   },
 
